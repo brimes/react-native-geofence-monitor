9c2576d8ba6f2f22b6f637e525b2a2a7675a1408
Bruno Lima <bruno@fidelize.com.br>
2017-05-25 23:54:05 -0300

    More tests

--- android/src/main/java/com/fidelize/geofence/GeofenceMonitorModule.java
+++ android/src/main/java/com/fidelize/geofence/GeofenceMonitorModule.java
@@ -1,6 +1,17 @@
 package com.fidelize.geofence;
 
-import android.os.Handler;
+import android.content.SharedPreferences;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.util.Log;
+import android.os.Bundle;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.app.NotificationCompat;
+import android.app.NotificationManager;
+import android.graphics.Color;
+import android.content.Context;
+
+
 import java.util.ArrayList;
 
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -9,18 +20,47 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.GeofencingApi;
 
-public class GeofenceMonitorModule extends ReactContextBaseJavaModule {
+public class GeofenceMonitorModule extends ReactContextBaseJavaModule implements
+        ConnectionCallbacks, OnConnectionFailedListener, ResultCallback<Status> {
 
-    private Handler handler;
     private ArrayList<Geofence> geofencesList;
     private ReactContext reactContext;
 
+    protected static final String TAG = "ReactNativeJS";
+
+    /**
+     * Provides the entry point to Google Play services.
+     */
+    protected GoogleApiClient mGoogleApiClient;
+
+    /**
+     * Used when requesting to add or remove geofences.
+     */
+    private PendingIntent mGeofencePendingIntent;
+
+    /**
+     * Used to persist application state about whether geofences were added.
+     */
+    private SharedPreferences mSharedPreferences;
+
     public GeofenceMonitorModule(ReactApplicationContext reactContext) {
         super(reactContext);
+        this.reactContext = reactContext;
         geofencesList = new ArrayList<Geofence>();
+        buildGoogleApiClient();
+        mGoogleApiClient.connect();
+
     }
 
     @Override
@@ -30,30 +70,117 @@ public class GeofenceMonitorModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void addLocation(String key, Double latitude, Double longitude) {
-        geofencesList.add(getGeofenceBuild(key, latitude, longitude));
+        geofencesList.add(new Geofence.Builder()
+                    // Set the request ID of the geofence. This is a string to identify this
+                    // geofence.
+                    .setRequestId(key)
+
+                    // Set the circular region of this geofence.
+                    .setCircularRegion(
+                            latitude,
+                            longitude,
+                            1000000
+                    )
+
+                    // Set the expiration duration of the geofence. This geofence gets automatically
+                    // removed after this period of time.
+                    .setExpirationDuration(120000)
+
+                    // Set the transition types of interest. Alerts are only generated for these
+                    // transition. We track entry and exit transitions in this sample.
+                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                            Geofence.GEOFENCE_TRANSITION_EXIT)
+
+                    // Create the geofence.
+                    .build());
     }
 
     @ReactMethod
-    public void removeAllLocations() {
-        //geofencesList.add(getGeofenceBuild(key, latitude, longitude));
+    public void notification() {
     }
 
-    private Geofence getGeofenceBuild(String key, Double latitude, Double longitude) {
-        Geofence location = new Geofence.Builder()
-        .setRequestId(key)
+    @ReactMethod
+    public String start() {
+        if (!mGoogleApiClient.isConnected()) {
+            Log.i(TAG, "Not Connected");
+            return "Not Connected";
+        }
 
-        .setCircularRegion(
-            latitude,
-            longitude,
-            100
-        )
-        .setExpirationDuration(1000 * 60 * 60 * 12)
-        .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
-            Geofence.GEOFENCE_TRANSITION_EXIT)
-        .build();
+        try {
+            Log.i(TAG, "Start OK");
+            LocationServices.GeofencingApi.addGeofences(
+                mGoogleApiClient,
+                // The GeofenceRequest object.
+                getGeofencingRequest(),
+                // A pending intent that that is reused when calling removeGeofences(). This
+                // pending intent is used to generate an intent when a matched geofence
+                // transition is observed.
+                getGeofencePendingIntent()
+            ).setResultCallback(this); // Result processed in onResult().
+        } catch (SecurityException securityException) {
+            // Catch exception generated if the app does not use ACCESS_FINE_LOCATION permission.
+            Log.i(TAG, "Error verify Android Manifest");
+        }
 
-        return location;
+        return "OK";
+
     }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(reactContext)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }    
+
+    /**
+    * Runs when a GoogleApiClient object successfully connects.
+    */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+       Log.i(TAG, "Connected to GoogleApiClient");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.i(TAG, "Connection suspended");
+    }
+
+    /**
+     * Gets a PendingIntent to send with the request to add or remove Geofences. Location Services
+     * issues the Intent inside this PendingIntent whenever a geofence transition occurs for the
+     * current list of geofences.
+     *
+     * @return A PendingIntent for the IntentService that handles geofence transitions.
+     */
+    private PendingIntent getGeofencePendingIntent() {
+        // Reuse the PendingIntent if we already have it.
+        if (mGeofencePendingIntent != null) {
+            return mGeofencePendingIntent;
+        }
+
+        Intent intent = new Intent(reactContext, GeofenceTransitionsIntentService.class);
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling
+        // addGeofences() and removeGeofences().
+        return PendingIntent.getService(reactContext, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    public void onResult(Status status) {
+        if (status.isSuccess()) {
+            Log.e(TAG, "Connected+");
+
+        } else {
+            Log.e(TAG, "Geofence Error code " + status.getStatusCode());
+        }
+    }    
 
     private GeofencingRequest getGeofencingRequest() {
         GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
@@ -61,4 +188,46 @@ public class GeofenceMonitorModule extends ReactContextBaseJavaModule {
         builder.addGeofences(geofencesList);
         return builder.build();
     }
+
+    /**
+     * Posts a notification in the notification bar when a transition is detected.
+     * If the user clicks the notification, control goes to the MainActivity.
+     */
+    private void sendNotification(String notificationDetails) {
+        // Create an explicit content Intent that starts the main Activity.
+        Intent notificationIntent = new Intent(reactContext, GeofenceMonitorModule.class);
+
+        // Construct a task stack.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(reactContext);
+
+        // Add the main Activity to the task stack as the parent.
+        //stackBuilder.addParentStack(GeofenceMonitorModule.class);
+
+        // Push the content Intent onto the stack.
+        stackBuilder.addNextIntent(notificationIntent);
+
+        // Get a PendingIntent containing the entire back stack.
+        PendingIntent notificationPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Get a notification builder that's compatible with platform versions >= 4
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(reactContext);
+
+        // Define the notification settings.
+        builder.setColor(Color.RED)
+            .setContentTitle(notificationDetails)
+            .setContentText("Teste Notification")
+            .setContentIntent(notificationPendingIntent);
+
+        // Dismiss notification once the user touches it.
+        builder.setAutoCancel(true);
+
+        // Get an instance of the Notification manager
+        NotificationManager mNotificationManager =
+                (NotificationManager) reactContext.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Issue the notification
+        mNotificationManager.notify(0, builder.build());
+    }
+
 }
